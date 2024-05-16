c527e57c9a7ab4b0a51728066f03d790ff5f0486
yukun <yukun.web@gmail.com>
2024-05-17 01:44:41 +0900

    年齢を誕生日から計算するように変更

--- class_and_instance.js
+++ class_and_instance.js
@@ -1,27 +1,34 @@
 class Person {
-  constructor (name, age) {
-    this.name = name
-    this.age = age
+  #name
+  #birthday
+
+  constructor (name, birthday) {
+    this.#name = name
+    this.#birthday = birthday
   }
 
-  ageUp () {
-    this.age++
+  get #age () {
+    const diff = new Date() - this.#birthday
+    const age = new Date(diff)
+    return Math.abs(age.getUTCFullYear() - 1970)
   }
 
   rename (newName) {
-    this.name = newName
+    this.#name = newName
   }
 
   greet () {
-    console.log(`Hello, my name is ${this.name}`)
+    console.log(`Hello, my name is ${this.#name}`)
   }
 
-  sayAge () {
-    console.log(`I am ${this.age} years old`)
+  introduceMySelf () {
+    console.log(`I am ${this.#age} years old`)
   }
 }
 
-const firstPerson = new Person('John', 20)
+const firstPerson = new Person('John', new Date('1994-03-28'))
 
 firstPerson.greet() // Hello, my name is John
-firstPerson.sayAge() // I am 20 years old
+firstPerson.rename('Jane')
+firstPerson.greet() // Hello, my name is Jane
+firstPerson.introduceMySelf() // I am xx years old
